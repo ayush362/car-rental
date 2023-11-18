2f3858299207003f4e92e2e3c7496e88c7fc2e3c
Sangeet Khatri <sangeet.khatri@outlook.com>
2023-11-18 23:25:37 +0530

    Fixing navbar, footer exports

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-const Footer = () => {
+export const Footer = () => {
   return <div>This is the footer</div>;
 };
-
-export default Footer;
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-const Navbar = () => {
+export const Navbar = () => {
   return <div>This is the navbar</div>;
 };
-
-export default Navbar;
